dca0c96634d85320667dd75012eaff656c4a5a85
Baraa710 <baraalchalabi@gmail.com>
2023-06-06 16:36:35 +0300

    update __init__.py to initialize logging in objects

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
+bcrypt = Bcrypt(app)
 CORS(app)
 
 from app import routes
