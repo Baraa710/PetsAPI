eb9aac330a37ca35b443341c8cc9a87729c35c68
Baraa710 <baraalchalabi@gmail.com>
2023-06-06 16:37:08 +0300

    include a functional login route

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 
-from flask import jsonify, request, abort
-from app import app, db
-from app.models import Pet
+from flask import jsonify, request, abort, redirect, url_for
+from app import app, db, bcrypt
+from app.models import Pet, User
 from flask_cors import cross_origin
-
+from flask_login import login_user
+from sqlalchemy import or_
 
 @app.route('/')
 def index():
@@ -12,13 +13,14 @@ def index():
 @cross_origin()
 @app.route('/pets', methods=['POST'])
 def create_pet():
+    """"""
     pet_data = request.json
 
     pet_name = pet_data['pet_name']
     pet_type = pet_data['pet_type']
     pet_age = pet_data['pet_age']
     pet_description = pet_data['pet_description']
-
+    # pet=Pet(**pet_data)
     pet = Pet(pet_name =pet_name , pet_type = pet_type, pet_age = pet_age, pet_description =pet_description)
     db.session.add(pet)
     db.session.commit()
@@ -26,8 +28,9 @@ def create_pet():
     return jsonify({"success":True, "response":"Pet "+pet_name+" added" })
 
 @cross_origin
-@app.route('/getpets',methods=['GET'])
+@app.route('/pets',methods=['GET'])
 def getpets():
+    """Returns a json string containing all pets in the database"""
     all_pets = []
     pets = Pet.query.all()
     for pet in pets:
@@ -52,6 +55,7 @@ def getpets():
 @cross_origin
 @app.route("/pets/<int:pet_id>", methods = ["PATCH"])
 def update_pet(pet_id):
+    """Takes pet id, update a pet age and/or description."""
     pet = Pet.query.get(pet_id)
     json_request = request.json
 
@@ -70,6 +74,7 @@ def update_pet(pet_id):
 @cross_origin
 @app.route("/pets/<int:pet_id>", methods = ["DELETE"])
 def delete_pet(pet_id):
+    """given pet id, this function removes the pet from the database"""
     pet = Pet.query.get(pet_id)
     if pet is None:
         abort(404)
@@ -77,4 +82,24 @@ def delete_pet(pet_id):
         db.session.delete(pet)
         db.session.commit()
         return jsonify({"success": True, "response": "Pet with id " +str(pet_id) +" deleted"})
+    
+
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    '''
+    Parses login information from json request. Aceepts both username or password. Returns a Json response 
+    with the logged in state (true or false) and reason for failed attempts
+    ''' 
+    json_request = request.json
+    
+    user = User.query.filter(or_(User.username == json_request.get('username'), User.email == json_request.get('email'))).first()
+    
+    if user:
+        if user.password==json_request['password']:
+            login_user(user)
+            return jsonify({"Logged in": True})
+        return jsonify({"Logged in": False, "response": "Invalid password"})
+    return jsonify({"Logged in":False, "response":"Invalid username"})
     
